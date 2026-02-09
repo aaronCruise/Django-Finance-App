02541bf1aad23602451b652651390efcecd4ed20
aaronCruise <aaron.m.cruz@rutgers.edu>
2026-02-09 17:19:09 -0500

    Add data download script

---
+++ download.py
@@ -0,0 +1,17 @@
+# Download stock data to CSV file
+import yfinance as yf
+import pandas as pd
+
+ticker = "VT"
+output_file = "data.csv"
+
+print(f"Downloading stock data for {ticker}...")
+stock = yf.Ticker(ticker)
+
+header = ["Open", "High", "Low", "Close", "Volume"]
+
+dataFrame = stock.history(period="1y")
+dataFrame["Date"] = dataFrame.index.date
+
+dataFrame.to_csv(output_file, columns=header)
+print(f"Successfully downloaded data to: {output_file}")
